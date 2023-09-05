a4c78618d2523f601288b5f44cae2707aa2bb610
jayphan14 <kha21052004@gmail.com>
2023-09-05 10:58:39 -0400

    update routing convention ;

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,13 +1,15 @@
 import express from 'express';
-import { registerWithEmail, loginWithEmail, googleRegister } from '../controllers/auth';
+import { registerWithEmail, loginWithEmail } from '../controllers/auth';
+import { logOutUser } from "../controllers/user";
+import { withPermissions } from '../middleware/middleware';
 
 const router = express.Router();
 
 // all the routes
 // POST requests
-router.post('/register/email', registerWithEmail); // registration flow
-router.post('/login/email', loginWithEmail); // login with email
-router.post('/register/google', googleRegister); // login with third-party
+router.post('/register', registerWithEmail); // registration flow
+router.post('/login', loginWithEmail); // login with email
+router.post('/logout', withPermissions(['View profile']), logOutUser);
 
 
 export default router;
--- src/routes/userProfileRoutes.ts
+++ src/routes/userProfileRoutes.ts
@@ -1,19 +1,19 @@
 import express, { Router } from 'express';
-import { getUserWithEmail, updatePassword, deleteAccount, logOutUser } from "../controllers/user";
+import { getUserWithEmail, updatePassword, deleteAccount, updateUser } from "../controllers/user";
 import { withPermissions } from '../middleware/middleware';
 
 const router = express.Router();
 
 // GET request to get one user's profile
-router.get('/get-user-profile/email', withPermissions(['View profile']), getUserWithEmail);
+// router.get('/get-user-profile/email', withPermissions(['View profile']), getUserWithEmail);
+router.get('/get', getUserWithEmail);
 
-// PATCH request to update the user's password
-router.patch('/update-password/email', withPermissions(["Update password"]), updatePassword);
+// PATCH request to update any fields
+router.patch('/update-password', withPermissions(["Update password"]), updatePassword);
 
+router.patch('/update', updateUser);
 // DELETE request to delete the user's account
-router.delete('/delete-account/email', withPermissions(["Delete account"]), deleteAccount);
+router.delete('/delete', withPermissions(["Delete account"]), deleteAccount);
 
-// POST request to log user out
-router.post('/log-out/email', withPermissions(['View profile']), logOutUser);
 
 export default router;
