b58f73c375dc686e6e19312bb184e50e0709f36b
Kenttrann2302 <110959350+Kenttrann2302@users.noreply.github.com>
2023-09-14 00:41:51 -0400

    finish working querying posts depend on the user's location

--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getPostById, getPostByUserId, createNewPost, deletePost, updatePost} from "../controllers/post";
+import { getPostById, getPostByUserId, createNewPost, deletePost, updatePost, getPostByLocation } from "../controllers/post";
 
 const router = express.Router();
 
 router.post('/create', createNewPost );
 router.get('/getPostByUserId', getPostByUserId);
+router.get('/getPostByLocation', getPostByLocation);
 router.get('/getPostById', getPostById);
 router.delete('/delete', deletePost);
 router.patch('/update', updatePost);
